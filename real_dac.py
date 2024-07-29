5cc0459fb7c190398b12213cc4b0fa14b297b69e
ayushchaudhary55 <ayushchaudhary0494@gmail.com>
2024-07-29 23:16:13 +0530

    Add files via upload

---
+++ real_dac.py
@@ -0,0 +1,232 @@
+
+{
+    "type": "Feature",
+      "id": "KilcGU_R90Wu5KR9lvRDYX-FAC8=",
+      "properties": {
+        "ref": "0",
+        "@spider": "real_dac",
+        "addr:full": "Rét­ság, Piac tér 16-17.",
+        "addr:postcode": "2651",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.13794, 47.92787]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "x35ek3OGWCLECDUIeuB2yTtyFB0=",
+      "properties": {
+        "ref": "1",
+        "@spider": "real_dac",
+        "addr:full": "Romhány, Zrínyi út 6.",
+        "addr:postcode": "2654",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.26331, 47.92191]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "WwkTs3PZq2adLxvOgr_S0ycLGlM=",
+      "properties": {
+        "ref": "2",
+        "@spider": "real_dac",
+        "addr:full": "Dunakeszi, Kossuth út 1.",
+        "addr:postcode": "2120",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.15092, 47.61602]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "cf9w9Qmipwm-d8WpYkmtW0flW7o=",
+      "properties": {
+        "ref": "3",
+        "@spider": "real_dac",
+        "addr:full": "Domaszék, Petőfi u. 6/A",
+        "addr:postcode": "6781",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.999685, 46.244884]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "OQgdEbMrZbONWiFFDoAofIHka1I=",
+      "properties": {
+        "ref": "4",
+        "@spider": "real_dac",
+        "addr:full": "Budapest, Katona József u. 54.",
+        "addr:postcode": "1213",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.0821, 47.4267]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "nnQD7Dn0jZphIGotSDlunb0lNCI=",
+      "properties": {
+        "ref": "5",
+        "@spider": "real_dac",
+        "addr:full": "Budapest, Kondorosi út 6.",
+        "addr:postcode": "1116",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.0425467, 47.4500341]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "OQwvywACybd_G2buzVP3MeNTVEw=",
+      "properties": {
+        "ref": "6",
+        "@spider": "real_dac",
+        "addr:full": "Gyömrő, Wesselényi u. 1.",
+        "addr:postcode": "2230",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.3937736, 47.4289623]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "qc_2GtVIRtVAWXXhSM6idI8kn4c=",
+      "properties": {
+        "ref": "7",
+        "@spider": "real_dac",
+        "addr:full": "Cegléd, Kazinczy u. 49.",
+        "addr:postcode": "2700",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.8033578, 47.1658253]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "79OOIFUVyumfapN5Zc7cpn7fhsA=",
+      "properties": {
+        "ref": "8",
+        "@spider": "real_dac",
+        "addr:full": "Vecsés, Bokor u. 2.",
+        "addr:postcode": "2220",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.2720537, 47.4036746]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "FQmtKMx63YcZL84RcWgrBlMrd9Q=",
+      "properties": {
+        "ref": "9",
+        "@spider": "real_dac",
+        "addr:full": "Tóalmás, Béke út 22/A",
+        "addr:postcode": "2252",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.6640374, 47.5087062]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "lZePRnsIpLp4cBua9yA7u8rlAVU=",
+      "properties": {
+        "ref": "10",
+        "@spider": "real_dac",
+        "addr:full": "Budapest, Nyírpalota u. 52.",
+        "addr:postcode": "1156",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.1392851, 47.5454604]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "u4EMatFOPwt3d6TggDni10XObnw=",
+      "properties": {
+        "ref": "11",
+        "@spider": "real_dac",
+        "addr:full": "Budapest, Egressy utca 68.",
+        "addr:postcode": "1149",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.1121189, 47.510477]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "PmKpc5Xg8kt3NisstuMk9kDN8Y0=",
+      "properties": {
+        "ref": "12",
+        "@spider": "real_dac",
+        "addr:full": "Tápiógyörgye, Szentháromság tér 3.",
+        "addr:postcode": "2767",
+        "addr:country": "Hungary",
+        "website": "https://real.hu/",
+        "brand": "Real"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.9482672, 47.3366473]
+      }
+    },
+{
+      "type": "Feature",
+      "id": "Ge1qbZnPpEBwLmkTdFNgTcApYrM=",
+      "properties": {
+        "ref": "13",
+        "@spider": "real_dac",
+        "addr:full": "Budapest, Ady Endre út 172.",
+       }
+       
+       }
